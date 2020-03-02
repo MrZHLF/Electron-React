5cabaeda6eed3aca5dabacbbcee5b2fdf2b267cc
MrZHLF <480970024@qq.com>
2020-03-02 18:33:36 +0800

    页面布局

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import SimpleMDE from 'react-simplemde-editor'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'easymde/dist/easymde.min.css'
 import { faPlus, faFileImport } from '@fortawesome/free-solid-svg-icons'
 
 import FileSearch from './components/FileSearch'
@@ -8,6 +10,7 @@ import FileList from './components/FileList'
 import defaultFiles from './utils/defaultFiles'
 import BottomBtn from './components/BottomBtn.jsx'
 import TabList from './components/TabList'
+
 function App() {
   return (
     <div className="App container-fluid px-0">
@@ -30,28 +33,39 @@ function App() {
             onSaveEdit={(id, newValue) => {
               console.log(newValue, id)
             }}
-          />
+          />{' '}
           <div className="row no-gutters">
             <div className="col-6">
-              <BottomBtn text="新建" colorClass="btn-primary" icon={faPlus} />
-            </div>
+              <BottomBtn text="新建" colorClass="btn-primary" icon={faPlus} />{' '}
+            </div>{' '}
             <div className="col-6">
               <BottomBtn
                 text="导入"
                 colorClass="btn-success"
                 icon={faFileImport}
-              />
-            </div>
-          </div>
+              />{' '}
+            </div>{' '}
+          </div>{' '}
         </div>{' '}
         <div className="col-9  right-panel">
           <TabList
             files={defaultFiles}
             activeId="1"
+            unsaveId={[1, 2, 3]}
             onTabClick={id => {
               console.log('tab', id)
             }}
-          />
+            onCloseTab={id => {
+              console.log('onCloseTab', id)
+            }}
+          />{' '}
+          <SimpleMDE
+            value={defaultFiles[1].body}
+            onChange={value => {
+              console.log(value)
+            }}
+            options={{ minHeight: '515px' }}
+          />{' '}
         </div>{' '}
       </div>{' '}
     </div>
--- src/components/TabList.jsx
+++ src/components/TabList.jsx
@@ -3,13 +3,17 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import './TabList.scss'
 const TabList = ({ files, activeId, unsaveId, onTabClick, onCloseTab }) => {
   return (
-    <ul className="nav nav-pills">
+    <ul className="nav nav-pills tablist-component">
       {files.map(file => {
+        const withUnsavedMark = unsaveId.includes(file.id)
         const fClassName = classNames({
           'nav-link': true,
-          active: file.id === activeId
+          active: file.id === activeId,
+          'nav-active': file.id === activeId,
+          'with-Unsaved': withUnsavedMark
         })
         return (
           <li className="nav-item" key={file.id}>
@@ -21,9 +25,18 @@ const TabList = ({ files, activeId, unsaveId, onTabClick, onCloseTab }) => {
               }}
             >
               {file.title}
-              <span className="ml-2">
+              <span
+                className="ml-2 close-icon"
+                onClick={e => {
+                  e.stopPropagation()
+                  onCloseTab(file.id)
+                }}
+              >
                 <FontAwesomeIcon size="lg" icon={faTimes} />
               </span>
+              {withUnsavedMark && (
+                <span className="rounded-circle ml-2 unsaved-icon"></span>
+              )}
             </a>
           </li>
         )
